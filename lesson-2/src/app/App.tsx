168be8d887ba2adeb837a450171ba0c99c7a4d74
Dmitriy Kozhushko <dvkozhushko@sberbank.ru>
2026-09-26 17:38:27 +0300

    [lesson-2] project structure refactoring

--- lesson-2/src/app/App.tsx
+++
@@ -1,5 +0,0 @@
-import { AppRouter } from './router'
-
-const App = () => <AppRouter />
-
-export default App
--- lesson-2/src/pages/tasks/ui/TaskPage.tsx
+++
@@ -1,9 +0,0 @@
-import { TaskWidget } from 'widgets/task'
-
-export const TaskPage = () => (
-  <main>
-    <h1>Мои задачи</h1>
-
-    <TaskWidget />
-  </main>
-)
